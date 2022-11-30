22c9d50e0e5b42caf7dd3fd79d1f9dcd268c8589
josefinhellgren <josefin.hellgren@gmail.com>
2022-11-30 12:21:13 +0100

    changed so the board is being cleared when someone wins

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-private var count : Int = 0
+
 
 class ViewController: UIViewController {
+    
     
     @IBOutlet weak var a1Button: UIButton!
     @IBOutlet weak var messager: UILabel!
@@ -22,115 +23,114 @@ class ViewController: UIViewController {
     @IBOutlet weak var c2Button: UIButton!
     @IBOutlet weak var c3Button: UIButton!
     var ticTacList = ["", "", "","","","","","",""]
+    var buttonLIST = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // gör en lista av alla knappar
+       
+      
+     
+        
+        buttonLIST.append(a1Button)
+        buttonLIST.append(a2Button)
+        buttonLIST.append(a3button)
+        buttonLIST.append(b1Button)
+        buttonLIST.append(b2Button)
+        buttonLIST.append(b3Button)
+        buttonLIST.append(c1Button)
+        buttonLIST.append(c2Button)
+        buttonLIST.append(c3Button)
+       
         
         
-        
+       
         
     }
     func winner(){
-        //save titleLabel symbols to list
-        // in list
-        //check result with list
-        //kör funktion när det är >3 symboler i listan
-        // switch case
+       //winning logic
         
-        
-        
-    }
-    
-    func clearBoard(){
-        //when list is full
-        }
-    
-    @IBAction func buttons(_ sender: UIButton) {
-        
-    if(sender.titleLabel?.text == nil ){
-            count += 1
-            if(count % 2 == 0){
-                
-                
-                sender.setTitle("O", for: .normal)
-                messager.text = "X´s turn"
-                
-                
-            }else{sender.setTitle("X", for: .normal)
-                messager.text = "O's turn"
-                ticTacList[sender.tag] = sender.titleLabel?.text ?? ""
-            }
-            }
-        
-        if(count % 2 == 0){
-            ticTacList[sender.tag] = "O"
-        }else{
-            ticTacList[sender.tag] = "X"
-        }
-        
-        
-        
-        /// vinst logic:
-    
         if(ticTacList[0] == "X" && ticTacList[1] == "X" && ticTacList[2] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[0] == "O" && ticTacList[1] == "O" && ticTacList[2] == "O" ) {
-            messager.text = "O Wins!"
-        
-        }
+            }else if(ticTacList[0] == "O" && ticTacList[1] == "O" && ticTacList[2] == "O" ) {
+            messager.text = "O Wins!"}
         
         if(ticTacList[3] == "X" && ticTacList[4] == "X" && ticTacList[5] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[3] == "O" && ticTacList[4] == "O" && ticTacList[5] == "O" ) {
-            messager.text = "O Wins!"
-        
-        }
-        
-        
+         }else if(ticTacList[3] == "O" && ticTacList[4] == "O" && ticTacList[5] == "O" ) {
+            messager.text = "O Wins!"}
         
         if(ticTacList[6] == "X" && ticTacList[7] == "X" && ticTacList[8] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[6] == "O" && ticTacList[7] == "O" && ticTacList[8] == "O" ) {
+           }else if(ticTacList[6] == "O" && ticTacList[7] == "O" && ticTacList[8] == "O" ) {
             messager.text = "O Wins!"
-        
-        }
+           }
         
         if(ticTacList[0] == "X" && ticTacList[3] == "X" && ticTacList[6] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[0] == "O" && ticTacList[3] == "O" && ticTacList[6] == "O" ) {
-            messager.text = "O Wins!"
-        
-        }
+            }else if(ticTacList[0] == "O" && ticTacList[3] == "O" && ticTacList[6] == "O" ) {
+            messager.text = "O Wins!"}
         
         if(ticTacList[1] == "X" && ticTacList[4] == "X" && ticTacList[7] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[1] == "O" && ticTacList[4] == "O" && ticTacList[7] == "O" ) {
-            messager.text = "O Wins!"
-        
-        }
+            }else if(ticTacList[1] == "O" && ticTacList[4] == "O" && ticTacList[7] == "O" ) {
+            messager.text = "O Wins!"}
         
         if(ticTacList[2] == "X" && ticTacList[5] == "X" && ticTacList[8] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[2] == "O" && ticTacList[5] == "O" && ticTacList[8] == "O" ) {
-            messager.text = "O Wins!"
-        
-        }
+            }else if(ticTacList[2] == "O" && ticTacList[5] == "O" && ticTacList[8] == "O" ) {
+            messager.text = "O Wins!"}
         
         if(ticTacList[2] == "X" && ticTacList[4] == "X" && ticTacList[6] == "X"){
             messager.text = "X wins"
-        }else if(ticTacList[2] == "O" && ticTacList[4] == "O" && ticTacList[6] == "O" ) {
-            messager.text = "O Wins!"
+           }else if(ticTacList[2] == "O" && ticTacList[4] == "O" && ticTacList[6] == "O" ) {
+            messager.text = "O Wins!"}
         
-        }
-        
+        clearBoard()
     }
     
     
     
+    func clearBoard(){
+        if(messager.text == "O Wins!" || messager.text == "X wins"){
+            
+            ticTacList = ["", "", "","","","","","",""]
+            for buttons in buttonLIST {
+                buttons.setTitle("", for: .normal)
+               
+        }
+           
+        
+            
+            
+            
+        }
+        }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        print("\(count)")
+        print("\(ticTacList)")
+    if(sender.titleLabel?.text == nil ){
+            count += 1
+            if(count % 2 == 0){
+             sender.setTitle("O", for: .normal)
+                ticTacList[sender.tag] = "O"
+                messager.text = "X´s turn"
+                
+            }else if (count % 2 != 0)
+        {sender.setTitle("X", for: .normal)
+                messager.text = "O's turn"
+                ticTacList[sender.tag] = "X"
+
+            }
+            }
+        
+        
+        
+        winner()
+        print("\(ticTacList)")
+        }
     
     
     
-    
-}
+    }
 
